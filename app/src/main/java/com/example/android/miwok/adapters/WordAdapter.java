f10be64e100fb9183d27e6ab8a2723d63c34c1fe
Lenya Hope <lehone4hope@gmail.com>
2018-10-30 15:18:14 +0100

    Added translation for colors, family members and phrases

--- app/src/main/java/com/example/android/miwok/adapters/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/adapters/WordAdapter.java
@@ -1,4 +1,4 @@
-package com.example.android.miwok;
+package com.example.android.miwok.adapters;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.example.android.miwok.R;
+import com.example.android.miwok.model.Word;
 
 import java.util.ArrayList;
 
