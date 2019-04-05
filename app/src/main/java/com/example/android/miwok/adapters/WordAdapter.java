9f0f60608cdc15079a7de10922de5e7d35aa2356
Lenya Hope <lehone4hope@gmail.com>
2019-04-05 09:57:52 +0100

    Display different background colors for the different category activities

--- app/src/main/java/com/example/android/miwok/adapters/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/adapters/WordAdapter.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.media.Image;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,15 +19,15 @@ import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    private int bgColor;
+    private int mColorResourceId;
 
-    public WordAdapter(Activity context, ArrayList<Word> words, int backgroundColor) {
+    public WordAdapter(Activity context, ArrayList<Word> words, int colorResourceId) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
-        bgColor = backgroundColor;
+        mColorResourceId = colorResourceId;
     }
 
     @Override
@@ -61,7 +62,8 @@ public class WordAdapter extends ArrayAdapter<Word> {
         }
 
         View textContainer = listItemView.findViewById(R.id.text_container);
-        textContainer.setBackgroundColor(bgColor);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        textContainer.setBackgroundColor(color);
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
