a3aa0a7b5c39a9a975976ef3eec28c0baf4704c3
Lenya Hope <lehone4hope@gmail.com>
2019-04-06 00:15:01 +0100

    Release media player when audio finish playing

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -58,9 +58,36 @@ public class NumbersActivity extends AppCompatActivity {
                 Word word = words.get(i);
                 Log.v("NumbersActivity", "Current Word: "+word);
 
+                releaseMediaPlayer();
+
                 mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceID());
                 mMediaPlayer.start();
+
+                mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        releaseMediaPlayer();
+                    }
+                });
             }
         });
+    }
+
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
